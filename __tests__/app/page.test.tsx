4cf311d5876e1c286878276923be1bde8156f8f1
Jens Kadenbach <gitdax@daxbau.net>
2023-06-30 08:34:10 +0200

    Init

---
+++ __tests__/app/page.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Home from '@/app/page'
+ 
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+ 
+    const heading = screen.getByRole('heading', {
+      name: /levain love/i,
+    })
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
